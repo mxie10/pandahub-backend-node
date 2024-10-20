eda82943f0e49bb5b721e1ea74eb90a8b9f9fcae
mxie10 <xieyz13@gmail.com>
2024-10-19 20:54:24 -0400

    fix timestamp issue

--- functions/controllers/eventsController/create.js
+++ functions/controllers/eventsController/create.js
@@ -1,8 +1,11 @@
-const { getFirestore, FieldValue } = require('firebase-admin/firestore');
+const { getFirestore, FieldValue, Timestamp } = require('firebase-admin/firestore');
 const db = getFirestore();
 
 const create = async (req, res) => {
   const body = req.body;
+  if (body.date) {
+    body.date = Timestamp.fromDate(new Date(body.date));
+  }
   body.updatedAt = FieldValue.serverTimestamp(); 
   const docRef = await db.collection('events').add(body);
   res.status(201).json(
--- functions/controllers/eventsController/listAll.js
+++ functions/controllers/eventsController/listAll.js
@@ -2,7 +2,7 @@ const { getFirestore } = require('firebase-admin/firestore');
 const db = getFirestore();
 
 const listAll = async (req, res) => {
-  const eventsSnapshot = await db.collection('events').get();
+  const eventsSnapshot = await db.collection('events').orderBy('date').get();
   const events = eventsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
   res.status(200).json(events);
   // handle errors in eventsRoute.js
